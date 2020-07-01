16f777c7f479a9fbc0d89a3dac40f2eaec7c8bd9
秀樹 林 <hayashi@pascal-co-ltd.co.jp>
2020-07-01 13:48:25 +0900

    ViewModelクラスの実装とUIプロパティのバインディング

--- MVVMbinding/MainWindow.xaml.cs
+++ MVVMbinding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,18 @@ namespace MVVMbinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly MainViewModel vm;
+
         public MainWindow()
         {
             InitializeComponent();
+            vm = new MainViewModel();
+            this.DataContext = vm;
         }
 
-        // MVVMパターンを使わないコーディング. Windows Form Applicationのコーディングスタイルとほとんど変わらない.
-        // UIで配置したコントロールのプロパティをこの部分で直接操作する（LabelStatus）
-        // UIでの操作をイベントハンドラで処理する
+        // MVVMパターンのうち、UIに配置したコントロールのプロパティをバインディングした例.
+        // UIで配置したコントロールのプロパティはViewModelクラスのプロパティに置き換えられる.
+        // UIでの操作は引き続きイベントハンドラで処理する
 
         /// <summary>
         /// ButtonChAクリック時の処理
@@ -36,7 +41,7 @@ namespace MVVMbinding
         /// <param name="e"></param>
         private void ButtonChA_Click(object sender, RoutedEventArgs e)
         {
-            LabelStatus.Content = "A";
+            vm.Status = "A";
         }
 
         /// <summary>
@@ -46,7 +51,40 @@ namespace MVVMbinding
         /// <param name="e"></param>
         private void ButtonChB_Click(object sender, RoutedEventArgs e)
         {
-            LabelStatus.Content = "B";
+            vm.Status = "B";
         }
     }
+
+    /// <summary>
+    /// MainWindowのViewModel. INotifyPropertyChangedインターフェースを継承
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        // ラベルのメンバーとプロパティ
+        private string _status = "Start";
+        public string Status
+        {
+            get { return this._status; }
+            set
+            {
+                if (value != this._status)
+                {
+                    this._status = value;
+                    this.RaisePropertyChanged(nameof(Status));
+                    return;
+                }
+            }
+        }
+
+        // InotifyPropertyChangedメンバー
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+
 }
